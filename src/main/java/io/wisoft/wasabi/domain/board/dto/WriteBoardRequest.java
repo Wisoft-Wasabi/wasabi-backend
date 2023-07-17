a4898bb74f1c1f6a52435a42a49fe76becd73106
Jinwon-Dev <yjw9424@naver.com>
2023-07-17 14:31:19 +0900

    feat: 게시글 작성 요청에 태그 추가

--- src/main/java/io/wisoft/wasabi/domain/board/dto/WriteBoardRequest.java
+++ src/main/java/io/wisoft/wasabi/domain/board/dto/WriteBoardRequest.java
@@ -2,11 +2,13 @@ package io.wisoft.wasabi.domain.board.dto;
 
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record WriteBoardRequest(
-        Long memberId,
+        @NotNull Long memberId,
         @NotBlank String title,
         @NotBlank String content,
+        @NotBlank String[] tags,
         @Nullable String[] imageUrls
 ) {
 }
