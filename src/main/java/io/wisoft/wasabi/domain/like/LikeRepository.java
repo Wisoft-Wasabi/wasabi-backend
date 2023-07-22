b0acc144c49c7755fb5dfbe98fcd082ae7869a24
Mingi Kim <migni4575@gmail.com>
2023-07-22 23:35:01 +0900

    feat: 좋아요 취소 persistenct layer 구현

--- src/main/java/io/wisoft/wasabi/domain/like/LikeRepository.java
+++ src/main/java/io/wisoft/wasabi/domain/like/LikeRepository.java
@@ -1,6 +1,14 @@
 package io.wisoft.wasabi.domain.like;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
+
+    @Modifying(clearAutomatically = true)
+    @Query("DELETE FROM Like like WHERE like.member.id= :memberId AND like.board.id= :boardId")
+    int deleteByMemberIdAndBoardId(@Param("memberId") Long memberId, @Param("boardId") Long boardId);
+
 }
