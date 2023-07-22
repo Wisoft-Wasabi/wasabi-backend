21f1169ef038be78260693d1729cc4633de9d2fa
Mingi Kim <migni4575@gmail.com>
2023-07-23 01:35:24 +0900

    feat: 좋아요 취소 web layer 구현

--- src/main/java/io/wisoft/wasabi/domain/auth/exception/LoginFailException.java
+++ src/main/java/io/wisoft/wasabi/domain/auth/exception/LoginFailException.java
@@ -3,7 +3,7 @@ package io.wisoft.wasabi.domain.auth.exception;
 import io.wisoft.wasabi.global.exception.BusinessException;
 import io.wisoft.wasabi.global.exception.ErrorType;
 
-public class LoginFailException extends BusinessException{
+public class LoginFailException extends BusinessException {
 
     public LoginFailException() { super(ErrorType.LOGIN_FAIL);}
 
