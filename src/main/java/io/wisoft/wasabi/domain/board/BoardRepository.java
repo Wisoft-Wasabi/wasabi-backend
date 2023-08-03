56f3bac55d76f58763924d5778391cc442a338ba
Jinwon-Dev <yjw9424@naver.com>
2023-08-03 23:30:03 +0900

    fix: 비효율적인 쿼리문 수정(#WA-112)

--- src/main/java/io/wisoft/wasabi/domain/board/BoardRepository.java
+++ src/main/java/io/wisoft/wasabi/domain/board/BoardRepository.java
@@ -1,6 +1,5 @@
 package io.wisoft.wasabi.domain.board;
 
-import org.hibernate.annotations.BatchSize;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
