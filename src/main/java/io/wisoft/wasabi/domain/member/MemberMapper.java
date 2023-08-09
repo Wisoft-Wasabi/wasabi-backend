59dd54be591f5f82d5d3491810b02039942b1c5c
Jinwon-Dev <yjw9424@naver.com>
2023-08-10 01:57:12 +0900

    feat: Part Validation을 위한 수정

--- src/main/java/io/wisoft/wasabi/domain/member/MemberMapper.java
+++ src/main/java/io/wisoft/wasabi/domain/member/MemberMapper.java
@@ -8,7 +8,6 @@ import io.wisoft.wasabi.domain.member.dto.UpdateMemberInfoResponse;
 import io.wisoft.wasabi.global.config.common.bcrypt.EncryptHelper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 @Component
 public class MemberMapper {
@@ -59,6 +58,10 @@ public class MemberMapper {
 
     public String convertEmptyToNull(final String value) {
         return StringUtils.hasText(value) ? null : value;
+    }
+
+    public UpdateMemberInfoResponse entityToUpdateMemberInfoResponse(final Member member) {
+        return new UpdateMemberInfoResponse(member.getId());
     }
 
     public ReadMemberInfoResponse entityToReadMemberInfoResponse(final Member member) {
