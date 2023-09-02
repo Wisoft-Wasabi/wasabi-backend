b3707d6df5de9f08fbb4789619666c2cf92303e4
leedongyeop <ldy_1204@naver.com>
2023-09-02 14:05:56 +0900

    refactor : 엔티티 단위 테스트를 규약에 맞게 리팩토링(#WA-145)

--- src/test/java/io/wisoft/wasabi/domain/board/BoardTest.java
+++ src/test/java/io/wisoft/wasabi/domain/board/BoardTest.java
@@ -1,11 +1,15 @@
 package io.wisoft.wasabi.domain.board;
 
 import autoparams.AutoSource;
+import autoparams.customization.Customization;
+import io.wisoft.wasabi.customization.NotSaveBoardCustomization;
 import io.wisoft.wasabi.domain.member.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
+
+import static org.assertj.core.api.Assertions.*;
 
 class BoardTest {
 
@@ -13,23 +17,19 @@ class BoardTest {
     @DisplayName("게시글 조회수 증가")
     class IncreaseView {
 
+        @DisplayName("게시글 조회시 조회수가 1 증가해야 한다.")
         @ParameterizedTest
         @AutoSource
-        @DisplayName("게시글 조회시 조회수가 1 증가해야 한다.")
-        public void increase_view_success(final Member member) {
+        @Customization(NotSaveBoardCustomization.class)
+        void increase_view_success(final Board board) {
 
-            //given -- 조건
-            final Board board = new Board(
-                    "title",
-                    "content",
-                    member
-            );
+            // given
 
-            //when -- 동작
+            // when
             board.increaseView();
 
-            //then -- 검증
-            Assertions.assertThat(board.getViews()).isEqualTo(1);
+            // then
+            assertThat(board.getViews()).isEqualTo(1);
         }
     }
 }
