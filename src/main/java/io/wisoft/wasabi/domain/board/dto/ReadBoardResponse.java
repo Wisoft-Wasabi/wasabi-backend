566c73733b723b240d9f627a8d4989bd018696a4
Jinwon-Dev <yjw9424@naver.com>
2023-09-10 23:38:59 +0900

    feat: 게시글에서 작성자 프로필 조회 기능 추가(#WA-153)

--- src/main/java/io/wisoft/wasabi/domain/board/dto/ReadBoardResponse.java
+++ src/main/java/io/wisoft/wasabi/domain/board/dto/ReadBoardResponse.java
@@ -1,17 +1,29 @@
 package io.wisoft.wasabi.domain.board.dto;
 
+import io.wisoft.wasabi.domain.member.Part;
 import io.wisoft.wasabi.domain.tag.persistence.Tag;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record ReadBoardResponse (
+public record ReadBoardResponse(
         Long id,
         String title,
         String content,
-        String writer,
+        Writer writer,
         LocalDateTime createdAt,
         int likeCount,
         int views,
         boolean isLike,
-        List<Tag> tags) { }
+        List<Tag> tags) {
+
+    public record Writer(
+            String email,
+            String name,
+            String referenceUrl,
+            Part part,
+            String organization,
+            String motto
+    ) {
+    }
+}
