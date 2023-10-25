d74eb813db3fdd8014d7521b40defae0ac70c89f
Donggwon-Seo <ehdrnjsdl920@naver.com>
2023-10-25 21:45:24 +0900

    feat: Nginx 무중단 배포 테스트(2)

--- src/main/java/io/wisoft/wasabi/WasabiApplication.java
+++ src/main/java/io/wisoft/wasabi/WasabiApplication.java
@@ -1,11 +1,18 @@
 package io.wisoft.wasabi;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class WasabiApplication {
+
+    public static final String APPLICATION_LOCATIONS =
+            "spring.config.location=classpath:application.yml," +
+            "classpath:application-real.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(WasabiApplication.class, args);
+        new SpringApplicationBuilder(WasabiApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 }
