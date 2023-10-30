32590f59747ed2cc4f9a34542985d9ac8f6e3088
leeseunghee00 <lsh9246@gmail.com>
2023-10-30 16:38:00 +0900

    feat: Nginx 무중단 배포 테스트(4)

--- src/main/java/io/wisoft/wasabi/WasabiApplication.java
+++ src/main/java/io/wisoft/wasabi/WasabiApplication.java
@@ -6,13 +6,13 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 @SpringBootApplication
 public class WasabiApplication {
 
-    public static final String APPLICATION_LOCATIONS =
-            "spring.config.location=classpath:application.yml," +
-            "classpath:application-real.yml";
+//    public static final String APPLICATION_LOCATIONS =
+//            "spring.config.location=classpath:application.yml," +
+//            "classpath:application-real.yml";
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(WasabiApplication.class)
-                .properties(APPLICATION_LOCATIONS)
+//                .properties(APPLICATION_LOCATIONS)
                 .run(args);
     }
 }
