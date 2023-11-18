8f0ddd8213daaf7e91b650bf4306dc32df078e49
leedongyeop <ldy_1204@naver.com>
2023-11-18 16:32:26 +0900

    refactor : 향상된 for-each문을 사용하던 SortType 검증 로직을 Stream으로 변환(#WA-197)

--- src/main/java/io/wisoft/wasabi/domain/board/BoardServiceImpl.java
+++ src/main/java/io/wisoft/wasabi/domain/board/BoardServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -102,12 +103,10 @@ public class BoardServiceImpl implements BoardService {
     }
 
     private BoardSortType validateSortType(final String sortBy) {
-        for (BoardSortType value : BoardSortType.values()) {
-            if (value.getSortType().equalsIgnoreCase(sortBy.toUpperCase())) {
-                return value;
-            }
-        }
-        throw BoardExceptionExecutor.BoardSortTypeInvalidException();
+        return Arrays.stream(BoardSortType.values())
+                .filter(boardSortType -> boardSortType.getSortType().equalsIgnoreCase(sortBy.toUpperCase()))
+                .findAny()
+                .orElseThrow(BoardExceptionExecutor::BoardSortTypeInvalidException);
     }
 
     @Override
